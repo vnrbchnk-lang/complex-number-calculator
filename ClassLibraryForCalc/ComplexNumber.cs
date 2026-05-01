d45958d6c450be583ca377549ba70c1472d270e1
Ivan Ryabchenko <vnrbchnk@gmail.com>
2026-05-01 12:35:29 +0600

    Initial commit: project

---
+++ ClassLibraryForCalc/ComplexNumber.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexCalculatorContracts
+{
+    
+    public interface IComplexNumber
+    {
+        double RPart { get; }
+
+        double ImPart { get; }
+
+        double Modul { get; }
+
+        IComplexNumber Plus(IComplexNumber other);
+
+        IComplexNumber Minus(IComplexNumber other);
+
+        IComplexNumber Mnoj(IComplexNumber other);
+
+        IComplexNumber Delen(IComplexNumber other);
+
+        bool Equals(IComplexNumber other);
+
+        bool Bigger(IComplexNumber other);
+
+        bool Smaller(IComplexNumber other);
+
+        bool BiggerOrEqual(IComplexNumber other);
+        
+        bool SmallerOrEqual(IComplexNumber other);
+
+        string ToString();
+    }
+}
+
+
+
---
+++ ClassLibraryForCalc/UIcalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComplexCalculatorContracts
+{
+    
+    public interface ICalculatorUI
+    {
+        
+        IComplexNumber ReadComplexNumber(string prompt);
+
+        string ReadOperation();
+
+        void Result(string result);
+
+        void Error(string message);
+
+        void Run();
+    }
+}
